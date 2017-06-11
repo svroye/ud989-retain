477bf7702e53bf1259a36b3238273e2346c4df2d
Van Roye <steven.van-roye@sogeti.com>
2017-06-11 12:24:13 +0200

    Add date to notes

--- js/retain.js
+++ js/retain.js
@@ -20,7 +20,8 @@ $(function(){
     var octopus = {
         addNewNote: function(noteStr) {
             model.add({
-                content: noteStr
+                content: noteStr,
+                date: Date.now()
             });
             view.render();
         },
@@ -52,7 +53,7 @@ $(function(){
             var htmlStr = '';
             octopus.getNotes().forEach(function(note){
                 htmlStr += '<li class="note">'+
-                        note.content +
+                        note.content + '<span class="note-date">'+ new Date(note.date) + '</span>'
                     '</li>';
             });
             this.noteList.html( htmlStr );
